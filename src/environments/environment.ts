bae7278546d3a2e70593b1b2c8d52ef31da9269d
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-10-05 11:45:42 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://hospital-management-app-backend-v5sb.onrender.com' // change to your prod URL on deploy
+  apiBaseUrl: 'https://hospital-management-app-backend-v5sb.onrender.com/api' // change to your prod URL on deploy
 };
